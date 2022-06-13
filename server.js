70699bcfcee668121f1a88153feb2dc830415a84
UntrustableRus <46432997+UntrustableRus@users.noreply.github.com>
2022-06-12 20:32:04 -0400

    log process

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(function(req, res, next) {
   next();
 });
 
+console.log(process)
+
 app.get('/usr/:id', (req, res) => {
   const cryptr = require("./modules/Cryptr.js")
   res.send(cryptr.encrypt(req.params.id));
