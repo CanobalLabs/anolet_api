7b6a326374c17852f426c9eeb0fc652900b222b1
UntrustableRus <46432997+UntrustableRus@users.noreply.github.com>
2022-07-16 01:03:08 -0400

    Give new users belongings

--- routes/login.js
+++ routes/login.js
@@ -17,13 +17,32 @@ router.post("/signup", validate(validation, {}, {}), async (req, res) => {
     }
     let uid = uuidv4();
     bcrypt.hash(req.body.password, 10, function (err, hash) {
-        new User({ id: uid, defaultRender: true, created: new Date(), username: req.body.username, password: hash, amulets: 100, about: "I don't have a bio, but I can change that!" }).save();
+        new User({
+            id: uid,
+            defaultRender: true,
+            created: new Date(),
+            username: req.body.username,
+            password: hash, amulets: 100,
+            about: "I don't have a bio, but I can change that!",
+            belongings: {
+                hats: ["a05b72b1-15b2-4d4a-b45e-1d1a9488bd4d"],
+                bodies: ["3d62ac6b-b48f-43ac-a8bf-f43040e75111"],
+                faces: ["0aff884e-112b-45dd-afd3-afa1ff3ec3c2"],
+                shoes: []
+            },
+            avatar: {
+                hats: ["a05b72b1-15b2-4d4a-b45e-1d1a9488bd4d"],
+                bodies: ["3d62ac6b-b48f-43ac-a8bf-f43040e75111"],
+                faces: ["0aff884e-112b-45dd-afd3-afa1ff3ec3c2"],
+                shoes: []
+            }
+        }).save();
         res.json({ token: GenerateToken(uid), error: false });
     });
 });
 
 router.post("/", validate(validation, {}, {}), async (req, res) => {
-    User.findOne({ username: { $regex : new RegExp(req.body.username, "i") } }).then(response => {
+    User.findOne({ username: { $regex: new RegExp(req.body.username, "i") } }).then(response => {
         if (response == null) return res.status(400).json({
             token: null,
             error: "An account with that username does not exist."
