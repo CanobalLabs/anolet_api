0daefde23596e0b0b45e4545e85c614263a1b04a
UntrustableRus <46432997+UntrustableRus@users.noreply.github.com>
2022-08-05 16:07:17 -0400

    Add games to backend

--- routes/game.js
+++ routes/game.js
@@ -2,6 +2,16 @@ const express = require('express');
 let router = express.Router();
 const Game = require("../models/game.js");
 
+router.route("/s").get((req, res) => {
+    // remember for frontend devs, pages start at 0 on the backend
+    var query = {};
+    var page = 0;
+    if (req.query.page) page = req.query.page
+    Game.find(query, undefined, { skip: 20 * page, limit: 20 }, function (err, results) {
+        res.json(results)
+    });
+});
+
 router.route("/:gameId").get((req, res) => {
     Game.findOne({ "id": req.params.gameId }).then(game => {
         delete game.gdp;
