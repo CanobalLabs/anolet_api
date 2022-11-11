549af61f813a1b577e3dd626c2e1a20d6de86d77
Rus <46432997+UntrustableRus@users.noreply.github.com>
2022-11-11 12:37:48 -0500

    why not work? add semicolon

--- routes/user.js
+++ routes/user.js
@@ -19,8 +19,8 @@ const endpoints = {
 router.route("/me").get((req, res) => {
     if (!res.locals.id) return res.send("Unauthorized");
     User.findOne({ id: res.locals.id }).then(user => {
-        user.userId = res.locals.id
         user.password = undefined;
+        user.userId = res.locals.id;
         res.json(user);
     });
 }).post(validate(validation, {}, {}), async (req, res) => {
