06f45a02160b01d555626b61d603ff81a06067b2
Rus <46432997+UntrustableRus@users.noreply.github.com>
2022-11-15 21:23:16 -0500

    Fix perm calculator?

--- modules/CalculatePermissions.js
+++ modules/CalculatePermissions.js
@@ -1,3 +1,3 @@
 module.exports = function(ranks) {
-return ranks.map(rank => require("../constants/ranks.json").find(id => id = rank).permissions).flat();
+return ranks.map(rank => require("../constants/ranks.json").find(({ id }) => id == rank).permissions).flat();
 }
