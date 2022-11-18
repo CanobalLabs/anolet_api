38b8d47902228272407d946a51317db4d59bca5e
Rus <46432997+UntrustableRus@users.noreply.github.com>
2022-11-18 12:17:14 -0500

    Remove mass upload fields

--- validation/item.js
+++ validation/item.js
@@ -2,11 +2,10 @@ const { Joi } = require('express-validation')
 
 module.exports = {
   body: Joi.object({
-    id: Joi.any(),
     name: Joi.string().min(1).max(255).required(),
     description: Joi.string().min(1).max(255).required(),
     type: Joi.string().valid('accessory', 'face', 'body', 'shoes').required(),
     price: Joi.number().min(0).required(),
-    anoletAccount: Joi.boolean().required()
+    anoletAccount: Joi.boolean()
   }),
 }
