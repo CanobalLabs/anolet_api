10e774fe1f1bfbb421a50c4c65e64285bb0d0a46
Rus <46432997+UntrustableRus@users.noreply.github.com>
2022-11-18 12:03:00 -0500

    add multi perms

--- middleware/Permission.js
+++ middleware/Permission.js
@@ -1,10 +1,10 @@
 const CalculatePermissions = require("../modules/CalculatePermissions.js");
 const User = require("../models/user.js");
-module.exports = function (permission) {
+module.exports = function (perm1, perm2) {
     return function (req, res, next) {
         if (!res.locals.id) return res.status(401).send();
         User.findOne({ "id": res.locals.id }).then(user => {
-            if (CalculatePermissions(user.ranks).includes(permission)) {
+            if (CalculatePermissions(user.ranks).includes(perm1) || CalculatePermissions(user.ranks).includes(perm2)) {
                 next()
             } else {
                 res.status(403).send()
