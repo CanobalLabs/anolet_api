2c55505fc8c3248ddd72a830793e6956c554dc0f
Rus <46432997+UntrustableRus@users.noreply.github.com>
2022-11-18 12:03:49 -0500

    send along permissions

--- middleware/Permission.js
+++ middleware/Permission.js
@@ -5,6 +5,7 @@ module.exports = function (perm1, perm2) {
         if (!res.locals.id) return res.status(401).send();
         User.findOne({ "id": res.locals.id }).then(user => {
             if (CalculatePermissions(user.ranks).includes(perm1) || CalculatePermissions(user.ranks).includes(perm2)) {
+                res.locals.permissions = CalculatePermissions(user.ranks)
                 next()
             } else {
                 res.status(403).send()
