96dc6daf4a2a842a40ccd0fa19e8bf27d5e6ae83
Bacon <iam@baconing.tk>
2023-07-14 03:08:46 -0400

    shortened if statements

--- modules/CheckUsername.js
+++ modules/CheckUsername.js
@@ -2,8 +2,7 @@ const User = require("../models/canobalUser.js");
 
 async function checkUsername(username) {
     return User.findOne({ "username": { $regex: new RegExp(username, "i") } }, "id").then(response => {
-        if (response == null) return false;
-        if (response != null) return response.id;
+        return response == null ? false : response.id;
     });
 }
 
